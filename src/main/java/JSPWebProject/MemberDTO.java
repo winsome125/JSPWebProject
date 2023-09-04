06d53e4bd007b9a6130adab212c1e054a297fc7d
winsome125 <dksjfkd@naver.com>
2023-09-04 17:51:21 +0900

    03_JSPWebProject_20230904

---
+++ src/main/java/JSPWebProject/MemberDTO.java
@@ -0,0 +1,83 @@
+package JSPWebProject;
+
+public class MemberDTO {
+
+	// 멤버변수
+	private String id;       // 아이디
+	private String pass;     // 비밀번호
+	private String name;     // 이름
+	private String telephon; // 전화번호
+	private String mobile;   // 핸드폰번호
+	private String email;    // 이메일
+	private String mailing;  // 이메일 수신 여부
+	private String zipcode;  // 우편번호
+	private String addr1;    // 주소
+	private String addr2;    // 상세주소
+	
+	
+	// getter/setter 정의
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPass() {
+		return pass;
+	}
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getTelephon() {
+		return telephon;
+	}
+	public void setTelephon(String telephon) {
+		this.telephon = telephon;
+	}
+	public String getMobile() {
+		return mobile;
+	}
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getMailing() {
+		return mailing;
+	}
+	public void setMailing(String mailing) {
+		this.mailing = mailing;
+	}
+	public String getZipcode() {
+		return zipcode;
+	}
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+	}
+	public String getAddr1() {
+		return addr1;
+	}
+	public void setAddr1(String addr1) {
+		this.addr1 = addr1;
+	}
+	public String getAddr2() {
+		return addr2;
+	}
+	public void setAddr2(String addr2) {
+		this.addr2 = addr2;
+	}
+	
+	
+	
+	
+}
